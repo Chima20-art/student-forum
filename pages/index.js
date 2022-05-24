d3c18e911f1613f70e9d0c73044c025547e21d2d
Chima20-art <69147358+Chima20-art@users.noreply.github.com>
2022-05-24 22:30:02 +0100

    added some

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,41 @@
 import Head from "next/head";
 import Image from "next/image";
-import styles from "../styles/Home.module.css";
+import {
+  useAuthUser,
+  withAuthUser,
+  withAuthUserTokenSSR,
+} from "next-firebase-auth";
 
-export default function Home() {
-  return <div className={styles.container}></div>;
-}
+const styles = {
+  content: {
+    padding: 32,
+  },
+  infoTextContainer: {
+    marginBottom: 32,
+  },
+};
+
+const Demo = () => {
+  const AuthUser = useAuthUser();
+  return (
+    <div>
+      <div style={styles.content}>
+        <div style={styles.infoTextContainer}>
+          <h3>Home</h3>
+          <p>
+            This page does not require authentication, so it won't redirect to
+            the login page if you are not signed in.
+          </p>
+          <p>
+            If you remove `getServerSideProps` from this page, it will be static
+            and load the authed user only on the client side.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export const getServerSideProps = withAuthUserTokenSSR()();
+
+export default withAuthUser()(Demo);
