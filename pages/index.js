7e865947db9306c5f0e217a41759537b63b9dda0
Chima20-art <69147358+Chima20-art@users.noreply.github.com>
2022-05-24 22:13:24 +0100

    al

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,22 @@
 import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
+import {
+  useAuthUser,
+  withAuthUser,
+  withAuthUserTokenSSR,
+} from "next-firebase-auth";
 
-export default function Home() {
-  return <div className={styles.container}></div>;
+function Home() {
+  const AuthUser = useAuthUser();
+
+  return (
+    <div className={styles.container}>
+      <p>Your email is {AuthUser.email ? AuthUser.email : "unknown"}.</p>
+    </div>
+  );
 }
+
+export const getServerSideProps = withAuthUserTokenSSR()();
+
+export default withAuthUser()(Home);
