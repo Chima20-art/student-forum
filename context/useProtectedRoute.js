447ec80ee8e35383458c6f628d1ec1ecb732c55c
taoufiq lotfi <leh.lotfi@gmail.com>
2022-05-25 15:56:32 +0100

    added loadingUser state

--- context/useProtectedRoute.js
+++ context/useProtectedRoute.js
@@ -12,7 +12,7 @@ const useProtectedRoute = () => {
     }
   }, [user, loadingUser]);
 
-  return user;
+  return [user, loadingUser];
 };
 
 export default useProtectedRoute;
--- pages/profile.js
+++ pages/profile.js
@@ -2,7 +2,7 @@ import { useRouter } from 'next/router';
 import useProtectedRoute from '../context/useProtectedRoute';
 
 const Profile = () => {
-  const user = useProtectedRoute();
+  const [user, loadingUser] = useProtectedRoute();
 
   return (
     <div>
