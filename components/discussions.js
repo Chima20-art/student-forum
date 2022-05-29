b6cb85a390e9384443c17471248f709bdec00333
Chima20-art <69147358+Chima20-art@users.noreply.github.com>
2022-05-29 23:29:13 +0100

    finished first part of discussion session.

--- components/discussions.js
+++ components/discussions.js
@@ -7,6 +7,10 @@ import MenuItem from '@mui/material/MenuItem';
 import FormControl from '@mui/material/FormControl';
 import Select from '@mui/material/Select';
 import React, { useState } from 'react';
+import PushPinIcon from '@mui/icons-material/PushPin';
+import { blue } from '@mui/material/colors';
+import { pink } from '@mui/material/colors';
+import ModeCommentIcon from '@mui/icons-material/ModeComment';
 
 export default function Discussions() {
   const [menuItem, setMenuItem] = useState('');
@@ -15,41 +19,71 @@ export default function Discussions() {
     setMenuItem(event.target.value);
   };
   return (
-    <Box
+    <Paper
+      elevation={3}
       container
       sx={{
         width: { xs: '95%', md: '85%' },
         margin: '20px auto ',
+        padding: '30px',
+        justifyContent: 'center',
       }}
     >
-      <Paper elevation={3}>
-        <Grid sx={{ padding: '30px' }}>
-          <Grid
-            key="category button"
-            container
-            sx={{
-              alignItems: 'center',
-              justifyContent: 'flex-end',
-            }}
-          >
-            <Typography sx={{}}>Sorted by:</Typography>
-            <FormControl variant="standard" sx={{ m: 1, minWidth: 120 }}>
-              <Select
-                labelId="demo-simple-select-standard-label"
-                id="demo-simple-select-standard"
-                value={menuItem}
-                onChange={handleChange}
-                label="category"
-                defaultValue={1}
-              >
-                <MenuItem value={1}>Newest</MenuItem>
-                <MenuItem value={2}>Most comments</MenuItem>
-                <MenuItem value={3}>Most Likes</MenuItem>
-              </Select>
-            </FormControl>
+      <Grid container>
+        <Grid
+          item
+          key="category button"
+          container
+          sx={{
+            alignItems: 'center',
+            justifyContent: 'flex-end',
+          }}
+        >
+          <Typography sx={{}}>Sorted by:</Typography>
+          <FormControl variant="standard" sx={{ m: 1, minWidth: 120 }}>
+            <Select
+              labelId="demo-simple-select-standard-label"
+              id="demo-simple-select-standard"
+              value={menuItem}
+              onChange={handleChange}
+              label="category"
+              defaultValue={1}
+            >
+              <MenuItem value={1}>Newest</MenuItem>
+              <MenuItem value={2}>Most comments</MenuItem>
+              <MenuItem value={3}>Most Likes</MenuItem>
+            </Select>
+          </FormControl>
+        </Grid>
+      </Grid>
+      <Grid
+        item
+        container
+        key="discussion"
+        sx={{
+          justifyContent: 'space-between',
+          padding: '10px 0px 20px 0px',
+
+          borderBottom: '1px solid #acacac',
+        }}
+      >
+        <Grid container direction="row" item xs={5}>
+          <Grid item>
+            <PushPinIcon sx={{ color: pink[500] }} />
+          </Grid>
+          <Grid>
+            <Typography sx={{ fontWeight: 'bold' }}>
+              This is a post title to be discussed...
+            </Typography>
+            <Typography sx={{ color: '#acacac', fontSize: '13px' }}>
+              Started about 6 years ago by Elisa M
+            </Typography>
           </Grid>
         </Grid>
-      </Paper>
-    </Box>
+        <Grid item xs={3} backgroundColor="yellow">
+          dsvsd
+        </Grid>
+      </Grid>
+    </Paper>
   );
 }
