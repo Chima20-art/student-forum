e512bd155a9b40e47203767566c9944693e9d2fa
Chima20-art <69147358+Chima20-art@users.noreply.github.com>
2022-05-30 13:49:47 +0100

    completed discussion card design.

--- components/discussions.js
+++ components/discussions.js
@@ -10,8 +10,15 @@ import React, { useState } from 'react';
 import PushPinIcon from '@mui/icons-material/PushPin';
 import { blue } from '@mui/material/colors';
 import { pink } from '@mui/material/colors';
+import { grey } from '@mui/material/colors';
 import ModeCommentIcon from '@mui/icons-material/ModeComment';
 import { makeStyles } from '@mui/styles';
+import ChatBubbleIcon from '@mui/icons-material/ChatBubble';
+import RemoveRedEyeIcon from '@mui/icons-material/RemoveRedEye';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import ShortcutIcon from '@mui/icons-material/Shortcut';
+import { Shortcut } from '@mui/icons-material';
+import EventNoteIcon from '@mui/icons-material/EventNote';
 export default function Discussions() {
   const [menuItem, setMenuItem] = useState('');
 
@@ -63,12 +70,18 @@ export default function Discussions() {
         key="discussion"
         sx={{
           justifyContent: 'space-between',
-          padding: '10px 0px 20px 0px',
+          padding: '10px 0px 0px 0px',
 
           borderBottom: '1px solid #acacac',
         }}
       >
-        <Grid container direction="row" item xs={5}>
+        <Grid
+          container
+          direction="row"
+          item
+          xs={5}
+          sx={{ alignItems: 'center' }}
+        >
           <Grid item>
             <PushPinIcon sx={{ color: pink[500] }} />
           </Grid>
@@ -82,8 +95,73 @@ export default function Discussions() {
             </Typography>
           </Grid>
         </Grid>
-        <Grid item xs={3} backgroundColor="yellow">
-          dsvsd
+        <Grid container item xs={3}>
+          <Grid
+            container
+            direction="column"
+            item
+            xs={5}
+            sx={{
+              justifyContent: 'flex-start',
+            }}
+          >
+            {' '}
+            <Grid
+              container
+              item
+              sx={{
+                justifyContent: 'center',
+                alignItems: 'center',
+              }}
+            >
+              {' '}
+              <ChatBubbleIcon />
+              <Typography sx={{ fontSize: '23px' }}>926</Typography>
+            </Grid>
+          </Grid>
+          <Grid item xs={7} container direction="column" sx={{}}>
+            <Grid
+              container
+              item
+              sx={{
+                alignItems: 'center',
+                fontSize: '14px',
+                color: grey[400],
+              }}
+            >
+              <RemoveRedEyeIcon
+                sx={{
+                  height: '25px',
+                  width: '15px',
+                  marginRight: '5px',
+                }}
+              />
+              <Typography sx={{ fontSize: '12px' }}>9800 views</Typography>
+            </Grid>
+            <Grid container item sx={{ alignItems: 'center' }}>
+              <FavoriteIcon
+                sx={{
+                  height: '25px',
+                  width: '15px',
+                  marginRight: '5px',
+                  color: grey[400],
+                }}
+              />
+              <Typography sx={{ fontSize: '12px', color: grey[400] }}>
+                59 likes
+              </Typography>
+            </Grid>
+            <Grid
+              container
+              item
+              sx={{ alignItems: 'center', color: grey[400] }}
+            >
+              <EventNoteIcon
+                sx={{ height: '25px', width: '15px', marginRight: '5px' }}
+              />
+              <Typography sx={{ fontSize: '12px' }}>4 mounths ago</Typography>
+            </Grid>
+          </Grid>
         </Grid>
       </Grid>
     </Paper>
