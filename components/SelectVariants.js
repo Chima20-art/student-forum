67119e6fd9edd8f853e30243fce31db26f53ce07
Chima20-art <69147358+Chima20-art@users.noreply.github.com>
2022-06-01 13:58:43 +0100

    added SelectVariants.js for categories.

---
+++ components/SelectVariants.js
@@ -0,0 +1,60 @@
+import * as React from 'react';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
+import { Grid } from '@mui/material';
+
+export default function SelectVariants() {
+  const categories = [
+    { name: 'Movies' },
+    { name: 'University' },
+    { name: 'Study Help' },
+    { name: 'Music' },
+    { name: 'Gaming' },
+    { name: 'Jobs' },
+    { name: 'Money' },
+    { name: 'Careers' },
+    { name: 'Sports & Health' },
+    { name: 'Hobbies' },
+    { name: 'Pop Culture' },
+    { name: 'Fashion & beauty' },
+  ];
+
+  const [category, setCategory] = React.useState('');
+
+  const handleChange = (event) => {
+    setCategory(event.target.value);
+  };
+
+  return (
+    <Grid sx={{ width: '70%', margin: 'auto', marginTop: '50px' }}>
+      <FormControl
+        variant="standard"
+        sx={{
+          width: '20%',
+        }}
+      >
+        <InputLabel id="demo-simple-select-standard-label">
+          Discussion Category
+        </InputLabel>
+        <Select
+          labelId="demo-simple-select-standard-label"
+          id="demo-simple-select-standard"
+          value={category}
+          onChange={handleChange}
+          label="Category"
+        >
+          <MenuItem value="">
+            <em>None</em>
+          </MenuItem>
+          {categories.map((category) => (
+            <MenuItem value={category.name} key={category.name}>
+              {category.name}
+            </MenuItem>
+          ))}
+        </Select>
+      </FormControl>
+    </Grid>
+  );
+}
