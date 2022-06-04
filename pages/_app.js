4c10cdeea3b7b71fd6672986015317447990c4fc
Chima20-art <69147358+Chima20-art@users.noreply.github.com>
2022-06-04 20:32:33 +0100

    fixed mui&next implementation error.

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,33 @@
-import '../styles/globals.css'
-import { SessionProvider } from "next-auth/react"
+import '../styles/globals.css';
+import { SessionProvider } from 'next-auth/react';
+import { ThemeProvider } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+import { CacheProvider } from '@emotion/react';
+import theme from '../utils/theme';
+import createEmotionCache from '../utils/createEmotionCache';
+import Head from 'next/head';
+
+const clientSideEmotionCache = createEmotionCache();
 
 function App({
   Component,
+  emotionCache = clientSideEmotionCache,
   pageProps: { session, ...pageProps },
 }) {
   return (
-    <SessionProvider session={session}>
-      <Component {...pageProps} />
-    </SessionProvider>
-  )
+    <CacheProvider value={emotionCache}>
+      <Head>
+        <meta name="viewport" content="initial-scale=1, width=device-width" />
+      </Head>
+      <ThemeProvider theme={theme}>
+        {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+        <CssBaseline />
+        <SessionProvider session={session}>
+          <Component {...pageProps} />
+        </SessionProvider>
+      </ThemeProvider>
+    </CacheProvider>
+  );
 }
 
-export default App
+export default App;
